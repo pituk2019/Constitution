b0b058342b951bd14678a3d3db28291c4bc70035
Vlad Minkov <vladislav.minkov@monterosa.co.uk>
2020-10-19 12:25:59 +0300

    Init

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import { h } from "preact";
+
+import Header from "./header";
+
+// Code-splitting is automated for `routes` directory
+
+const App = () => (
+  <div id="app">
+    <Header />
+  </div>
+);
+
+export default App;
